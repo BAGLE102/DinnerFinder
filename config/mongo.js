595b3a3e47a7bc45d8c3f1f4151114d7cb05127b
BAGLE102 <146699756+BAGLE102@users.noreply.github.com>
2025-10-11 16:56:34 +0800

    Update mongo.js

--- config/mongo.js
+++ config/mongo.js
@@ -2,19 +2,19 @@ import mongoose from 'mongoose';
 
 export default async function connectMongoDB() {
   const uri = process.env.MONGO_URI;
-  const dbName = process.env.MONGO_DB_NAME; // 建議設成 line_dinner
+  const dbName = process.env.MONGO_DB_NAME; // 建議設 line_dinner
 
   console.log('[Mongo] connecting to:', uri?.replace(/:\/\/.*@/, '://***@'));
   console.log('[Mongo] dbName option =', dbName || '(from URI)');
 
   await mongoose.connect(uri, {
-    dbName,                       // ← 沒在 URI 指 DB 名時，**一定**要指定
+    dbName,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   });
 
   mongoose.connection.on('connected', () => {
     console.log('=== MongoDB connected ===');
-    console.log('[Mongo] effective db name =', mongoose.connection.name); // ← 實際 DB
+    console.log('[Mongo] effective db name =', mongoose.connection.name);
   });
 }
