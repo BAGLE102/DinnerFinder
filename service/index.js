46187e62000250753360f2e4c946a53f41b93825
BAGLE102 <146699756+BAGLE102@users.noreply.github.com>
2025-10-11 19:08:13 +0800

    Update index.js

--- service/index.js
+++ service/index.js
@@ -1,5 +1,6 @@
+// src/service/index.js
 import updateUserLocation from './updateUserLocation.js';
-import randomRestaurant from './randomRestaurant.js';
+import randomRestaurant from './randomRestaurant.js'; // 若你已不使用舊 random，可暫時保留
 import exploreRestaurant from './exploreRestaurant.js';
 import getMyRestaurant from './getMyRestaurant.js';
 import chooseRestaurant from './chooseRestaurant.js';
