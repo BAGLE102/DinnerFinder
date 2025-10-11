26be849f98b7043996c0b9e1eab13b867921c5eb
BAGLE102 <146699756+BAGLE102@users.noreply.github.com>
2025-10-11 16:58:47 +0800

    Update index.js

--- service/index.js
+++ service/index.js
@@ -1,27 +1,17 @@
-import help from './help.js'
-import echo from './echo.js'
-import followBot from './followBot.js'
-import updateUserLocation from './updateUserLocation.js'
-import addRestaurant from './addRestaurant.js'
-import getMyRestaurant from './getMyRestaurant.js'
-import removeRestaurant from './removeRestaurant.js'
-import chooseRestaurant from './chooseRestaurant.js'
-import randomRestaurant from './randomRestaurant.js'
-import exploreRestaurant from './exploreRestaurant.js'
-import setJoinCode from './setJoinCode.js'
-import checkJoinCodesExist from './checkJoinCodesExist.js'
+import updateUserLocation from './updateUserLocation.js';
+import randomRestaurant from './randomRestaurant.js';
+import exploreRestaurant from './exploreRestaurant.js';
+import getMyRestaurant from './getMyRestaurant.js';
+import chooseRestaurant from './chooseRestaurant.js';
+import addRestaurant from './addRestaurant.js';
+import removeRestaurant from './removeRestaurant.js';
 
 export default {
-  help,
-  echo,
-  followBot,
   updateUserLocation,
-  addRestaurant,
-  getMyRestaurant,
-  removeRestaurant,
-  chooseRestaurant,
   randomRestaurant,
   exploreRestaurant,
-  setJoinCode,
-  checkJoinCodesExist
-}
+  getMyRestaurant,
+  chooseRestaurant,
+  addRestaurant,
+  removeRestaurant
+};
